4ab4efe1e1198ad2fd5cec7074189381416541a9
tomayac <steiner.thomas@gmail.com>
2016-01-12 13:06:21 +0100

    Added three new functions: - WIKIINBOUNDLINKS - WIKIOUTBOUNDLINKS - WIKIGEOCOORDINATES

--- Wikipedia.gs.js
+++ Wikipedia.gs.js
@@ -7,13 +7,16 @@
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
-
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+/* globals UrlFetchApp, XmlService */
+/* jshint -W004 */
 
 /**
  * Returns Wikipedia synonyms (redirects) for a Wikipedia article
@@ -22,6 +25,7 @@
  * @return {Array<string>} The list of synonyms
  */
 function WIKISYNONYMS(article) {
+  'use strict';
   if (!article) {
     return '';
   }
@@ -65,6 +69,7 @@ function WIKISYNONYMS(article) {
  */
 function WIKITRANSLATE(article, opt_targetLanguages, opt_returnAsObject,
     opt_skipHeader) {
+  'use strict';
   if (!article) {
     return '';
   }
@@ -135,210 +140,7 @@ function WIKITRANSLATE(article, opt_targetLanguages, opt_returnAsObject,
  * @return {Array<string>} The list of translations and synonyms
  */
 function WIKIEXPAND(article, opt_targetLanguages, opt_returnAsObject) {
-  if (!article) {
-    return '';
-  }
-  var results = opt_returnAsObject ? {} : [];
-  try {
-    var language = article.split(':')[0];
-    var title = article.split(':')[1];
-    if (!title) {
-      return '';
-    }
-    opt_targetLanguages = opt_targetLanguages || [];
-    opt_targetLanguages = Array.isArray(opt_targetLanguages) ?
-        opt_targetLanguages : [opt_targetLanguages];
-    var temp = {};
-    opt_targetLanguages.forEach(function(lang) {
-      temp[lang] = true;
-    });
-    opt_targetLanguages = Object.keys(temp);
-    var translations = WIKITRANSLATE(article, opt_targetLanguages, true);
-    var i = 0;
-    for (var lang in translations) {
-      var synonyms = WIKISYNONYMS(lang + ':' + translations[lang]);
-      if (opt_returnAsObject) {
-        results[lang] = [translations[lang]].concat(synonyms);
-      } else {
-        results[i] = [lang].concat(([translations[lang]].concat(synonyms)));
-      }
-      i++;
-    }
-  } catch (e) {
-    // no-op
-  }
-  return opt_returnAsObject ? results : results;
-}
-
-/**
- * @license
- * Copyright 2015 Thomas Steiner (@tomayac). All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
-
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-/**
- * Returns Google Suggest results for the given keyword
- *
- * @param {string} keyword The keyword to get suggestions for
- * @param {string=} opt_language The language to get suggestions in, defaults to "en" (optional)
- * @return {Array<string>} The list of suggestions
- */
-function GOOGLESUGGEST(keyword, opt_language) {
-  if (!keyword) {
-    return '';
-  }
-  opt_language = opt_language || 'en';
-  var results = [];
-  try {
-    var url = 'http://suggestqueries.google.com/complete/search' +
-        '?output=toolbar' +
-        '&hl=' + opt_language +
-        '&q=' + encodeURIComponent(keyword);
-    var xml = UrlFetchApp.fetch(url).getContentText();
-    var document = XmlService.parse(xml);
-    var entries = document.getRootElement().getChildren('CompleteSuggestion');
-    for (var i = 0; i < entries.length; i++) {
-      var text = entries[i].getChild('suggestion').getAttribute('data')
-          .getValue();
-      results[i] = text;
-    }
-  } catch (e) {
-    // no-op
-  }
-  return results.length > 0 ? results : '';
-}
-
-/**
- * Returns Wikipedia synonyms (redirects) for a Wikipedia article
- *
- * @param {string} article The Wikipedia article in the format "language:Article_Title" ("de:Berlin") to get synonyms for
- * @return {Array<string>} The list of synonyms
- */
-function WIKISYNONYMS(article) {
-  if (!article) {
-    return '';
-  }
-  var results = [];
-  try {
-    var language = article.split(':')[0];
-    var title = article.split(':')[1];
-    if (!title) {
-      return '';
-    }
-    var url = 'https://' + language + '.wikipedia.org/w/api.php' +
-        '?action=query' +
-        '&blnamespace=0' +
-        '&list=backlinks' +
-        '&blfilterredir=redirects' +
-        '&bllimit=max' +
-        '&format=xml' +
-        '&bltitle=' + title.replace(/\s/g, '_');
-    var xml = UrlFetchApp.fetch(url).getContentText();
-    var document = XmlService.parse(xml);
-    var entries = document.getRootElement().getChild('query')
-        .getChild('backlinks').getChildren('bl');
-    for (var i = 0; i < entries.length; i++) {
-      var text = entries[i].getAttribute('title').getValue();
-      results[i] = text;
-    }
-  } catch (e) {
-    // no-op
-  }
-  return results.length > 0 ? results : '';
-}
-
-/**
- * Returns Wikipedia translations (language links) for a Wikipedia article
- *
- * @param {string} article The Wikipedia article in the format "language:Article_Title" ("de:Berlin") to get translations for
- * @param {Array<string>=} opt_targetLanguages The list of languages to limit the results to (optional)
- * @param {boolean=} opt_returnAsObject Whether to return the results as an object, defaults to false (optional)
- * @param {boolean=} opt_skipHeader Whether to skip the header, defaults to false (optional)
- * @return {Array<string>} The list of translations
- */
-function WIKITRANSLATE(article, opt_targetLanguages, opt_returnAsObject,
-    opt_skipHeader) {
-  if (!article) {
-    return '';
-  }
-  var results = {};
-  opt_targetLanguages = opt_targetLanguages || [];
-  opt_targetLanguages = Array.isArray(opt_targetLanguages) ?
-      opt_targetLanguages : [opt_targetLanguages];
-  var temp = {};
-  opt_targetLanguages.forEach(function(lang) {
-    temp[lang] = true;
-  });
-  opt_targetLanguages = Object.keys(temp);
-  try {
-    var language = article.split(':')[0];
-    var title = article.split(':')[1];
-    if (!title) {
-      return '';
-    }
-    opt_targetLanguages.forEach(function(targetLanguage) {
-      if (targetLanguage) {
-        results[targetLanguage] = title.replace(/_/g, ' ');
-      }
-    });
-    var url = 'https://' + language + '.wikipedia.org/w/api.php' +
-        '?action=query' +
-        '&prop=langlinks' +
-        '&format=xml' +
-        '&lllimit=max' +
-        '&titles=' + title.replace(/\s/g, '_');
-    var xml = UrlFetchApp.fetch(url).getContentText();
-    var document = XmlService.parse(xml);
-    var entries = document.getRootElement().getChild('query').getChild('pages')
-        .getChild('page').getChild('langlinks').getChildren('ll');
-    var targetLanguagesSet = opt_targetLanguages.length > 0;
-    for (var i = 0; i < entries.length; i++) {
-      var text = entries[i].getText();
-      var lang = entries[i].getAttribute('lang').getValue();
-      if ((targetLanguagesSet) && (opt_targetLanguages.indexOf(lang) === -1)) {
-        continue;
-      }
-      results[lang] = text;
-    }
-    title = title.replace(/_/g, ' ');
-    results[language] = title;
-  } catch (e) {
-    // no-op
-  }
-  if (opt_returnAsObject) {
-    return results;
-  }
-  var arrayResults = [];
-  for (var lang in results) {
-    if (opt_skipHeader) {
-      arrayResults.push(results[lang]);
-     } else {
-       arrayResults.push([lang, results[lang]]);
-     }
-  }
-  return arrayResults.length > 0 ? arrayResults : '';
-}
-
-/**
- * Returns Wikipedia translations (language links) and synonyms (redirects) for a Wikipedia article
- *
- * @param {string} article The Wikipedia article in the format "language:Article_Title" ("de:Berlin") to get translations and synonyms for
- * @param {Array<string>=} opt_targetLanguages The list of languages to limit the results to (optional)
- * @param {boolean=} opt_returnAsObject Whether to return the results as an object, defaults to false (optional)
- * @return {Array<string>} The list of translations and synonyms
- */
-function WIKIEXPAND(article, opt_targetLanguages, opt_returnAsObject) {
+  'use strict';
   if (!article) {
     return '';
   }
@@ -381,6 +183,7 @@ function WIKIEXPAND(article, opt_targetLanguages, opt_returnAsObject) {
  * @return {Array<string>} The list of category members
  */
 function WIKICATEGORYMEMBERS(category) {
+  'use strict';
   if (!category) {
     return '';
   }
@@ -397,7 +200,7 @@ function WIKICATEGORYMEMBERS(category) {
         '&cmlimit=max' +
         '&cmprop=title' +
         '&cmtype=subcat%7Cpage' +
-        '&format=xml'+
+        '&format=xml' +
         '&cmnamespace=0' +
         '&cmtitle=' + encodeURIComponent(title.replace(/\s/g, '_'));
     var xml = UrlFetchApp.fetch(url).getContentText();
@@ -421,6 +224,7 @@ function WIKICATEGORYMEMBERS(category) {
  * @return {Array<string>} The list of subcategories
  */
 function WIKISUBCATEGORIES(category) {
+  'use strict';
   if (!category) {
     return '';
   }
@@ -437,7 +241,7 @@ function WIKISUBCATEGORIES(category) {
         '&cmlimit=max' +
         '&cmprop=title' +
         '&cmtype=subcat%7Cpage' +
-        '&format=xml'+
+        '&format=xml' +
         '&cmnamespace=14' +
         '&cmtitle=' + encodeURIComponent(title.replace(/\s/g, '_'));
     var xml = UrlFetchApp.fetch(url).getContentText();
@@ -455,6 +259,123 @@ function WIKISUBCATEGORIES(category) {
 }
 
 /**
+ * Returns Wikipedia inbound links for a Wikipedia article
+ *
+ * @param {string} article The Wikipedia article in the format "language:Article_Title" ("de:Berlin") to get inbound links for
+ * @return {Array<string>} The list of inbound links
+ */
+function WIKIINBOUNDLINKS(article) {
+  'use strict';
+  if (!article) {
+    return '';
+  }
+  var results = [];
+  try {
+    var language = article.split(':')[0];
+    var title = article.split(':')[1];
+    if (!title) {
+      return '';
+    }
+    var url = 'https://' + language + '.wikipedia.org/w/api.php' +
+        '?action=query' +
+        '&list=backlinks' +
+        '&bllimit=max' +
+        '&blnamespace=0' +
+        '&format=xml' +
+        '&bltitle=' + title.replace(/\s/g, '_');
+    var xml = UrlFetchApp.fetch(url).getContentText();
+    var document = XmlService.parse(xml);
+    var entries = document.getRootElement().getChild('query')
+        .getChild('backlinks').getChildren('bl');
+    for (var i = 0; i < entries.length; i++) {
+      var text = entries[i].getAttribute('title').getValue();
+      results[i] = text;
+    }
+  } catch (e) {
+    // no-op
+  }
+  return results.length > 0 ? results : '';
+}
+
+/**
+ * Returns Wikipedia outbound links for a Wikipedia article
+ *
+ * @param {string} article The Wikipedia article in the format "language:Article_Title" ("de:Berlin") to get outbound links for
+ * @return {Array<string>} The list of outbound links
+ */
+function WIKIOUTBOUNDLINKS(article) {
+  'use strict';
+  if (!article) {
+    return '';
+  }
+  var results = [];
+  try {
+    var language = article.split(':')[0];
+    var title = article.split(':')[1];
+    if (!title) {
+      return '';
+    }
+    var url = 'https://' + language + '.wikipedia.org/w/api.php' +
+        '?action=query' +
+        '&prop=links' +
+        '&plnamespace=0' +
+        '&format=xml' +
+        '&pllimit=max' +
+        '&titles=' + title.replace(/\s/g, '_');
+    var xml = UrlFetchApp.fetch(url).getContentText();
+    var document = XmlService.parse(xml);
+    var entries = document.getRootElement().getChild('query').getChild('pages')
+        .getChild('page').getChild('links').getChildren('pl');
+    for (var i = 0; i < entries.length; i++) {
+      var text = entries[i].getAttribute('title').getValue();
+      results[i] = text;
+    }
+  } catch (e) {
+    // no-op
+  }
+  return results.length > 0 ? results : '';
+}
+
+/**
+ * Returns Wikipedia geocoordinates for a Wikipedia article
+ *
+ * @param {string} article The Wikipedia article in the format "language:Article_Title" ("de:Berlin") to get geocoordinates for
+ * @return {Array<number>} The latitude and longitude
+ */
+function WIKIGEOCOORDINATES(article) {
+  'use strict';
+  if (!article) {
+    return '';
+  }
+  var results = [];
+  try {
+    var language = article.split(':')[0];
+    var title = article.split(':')[1];
+    if (!title) {
+      return '';
+    }
+    var url = 'https://en.wikipedia.org/w/api.php' +
+        '?action=query' +
+        '&prop=coordinates' +
+        '&format=xml' +
+        '&colimit=max' +
+        '&coprimary=primary' +
+        '&titles=' + title.replace(/\s/g, '_');
+    var xml = UrlFetchApp.fetch(url).getContentText();
+    var document = XmlService.parse(xml);
+    var coordinates = document.getRootElement().getChild('query')
+        .getChild('pages').getChild('page').getChild('coordinates')
+        .getChild('co');
+    var latitude = coordinates.getAttribute('lat').getValue();
+    var longitude = coordinates.getAttribute('lon').getValue();
+    results = [latitude, longitude];
+  } catch (e) {
+    // no-op
+  }
+  return results.length > 0 ? results : '';
+}
+
+/**
  * Returns Google Suggest results for the given keyword
  *
  * @param {string} keyword The keyword to get suggestions for
@@ -462,13 +383,14 @@ function WIKISUBCATEGORIES(category) {
  * @return {Array<string>} The list of suggestions
  */
 function GOOGLESUGGEST(keyword, opt_language) {
+  'use strict';
   if (!keyword) {
     return '';
   }
   opt_language = opt_language || 'en';
   var results = [];
   try {
-    var url = 'http://suggestqueries.google.com/complete/search' +
+    var url = 'https://suggestqueries.google.com/complete/search' +
         '?output=toolbar' +
         '&hl=' + opt_language +
         '&q=' + encodeURIComponent(keyword);
