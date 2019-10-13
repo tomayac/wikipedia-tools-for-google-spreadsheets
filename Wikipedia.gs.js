1adb0b00d5b76f75c7eee6d6f01119e1505b20a1
Simon Legner <Simon.Legner@gmail.com>
2019-10-13 16:07:39 +0200

    Add WIKIDATASEARCH to search for labels/aliases
    The existing function WIKIDATAQID searches based on Wikipedia articles
    and fails if the search language does not have a corresponding article.
    
    The added WIKIDATASEARCH function makes use of the wbsearch query API of
    Wikidata.

--- Wikipedia.gs.js
+++ Wikipedia.gs.js
@@ -1446,6 +1446,46 @@ function WIKIDATAQID(article) {
 }
 
 /**
+ * Searches for Wikidata entities using Wikidata labels and aliases.
+ *
+ * @param {string} search The search string in the format "language:Query" ("de:Berlin") to get the Wikidata qid for.
+ * @return {string} The Wikidata qid.
+ * @customfunction
+ */
+function WIKIDATASEARCH(search) {
+  'use strict';
+  if (!search) {
+    return '';
+  }
+  var results = [];
+  try {
+    var wbslanguage;
+    var wbssearch;
+    if (search.indexOf(':') !== -1) {
+      wbslanguage = search.split(/:(.+)?/)[0];
+      wbssearch = search.split(/:(.+)?/)[1];
+    } else {
+      wbslanguage = DEFAULT_LANGUAGE;
+      wbssearch = search;
+    }
+    if (!wbssearch) {
+      return '';
+    }
+    var url = 'https://www.wikidata.org/w/api.php' +
+        '?action=query' +
+        '&list=wbsearch' +
+        '&wbslanguage=' + wbslanguage +
+        '&format=json' +
+        '&wbssearch=' + encodeURIComponent(wbssearch);
+    var json = JSON.parse(UrlFetchApp.fetch(url, HEADERS).getContentText());
+    results[0] = json.query.wbsearch[0].title;
+  } catch (e) {
+    // no-op
+  }
+  return results.length > 0 ? results : '';
+}
+
+/**
  * Returns the labels for a Wikidata item.
  *
  * @param {string} qid The Wikidata item's qid to get the labels for.
